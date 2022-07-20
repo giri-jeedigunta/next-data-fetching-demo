b8f71ec86a19e8cadb234dd1d068de14e7c69b0e
Venkata Giribabu Jeedigunta <venjeedi@WKMZT5435E01.global.publicisgroupe.net>
2022-07-20 13:18:21 +0200

    feat: added simple navigation to jump btwn the pages

--- components/list.js
+++ components/list.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 export default function List({data}) {
     return (
         <>
           <ol>{data.map((e) => (
             <li key={e.id}>{e.name}</li>
           ))}</ol>
+          <Link href="/">More Rendering Examples</Link>
         </>
       );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
-export default function Home() {
-  return <div>Index Page</div>;
-}
+import Link from 'next/link'
 
-// this page is unused
+export default function Home() {
+  return (
+    <>
+    <h2>NextJS Page Rendering SSR vs SSG vs ISR</h2>
+    <ul>
+      <li>
+        <Link href="/ssr">
+          <a>SSR</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ssg">
+          <a>SSG</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/isr">
+          <a>ISR</a>
+        </Link>
+      </li>      
+      <li>
+        <Link href="/isr-on-demand">
+          <a>ISR On-demand</a>
+        </Link>
+      </li>            
+      <li>
+        <Link href="/slug-example/hello-world">
+          <a>SLUG + SSR</a>
+        </Link>
+      </li>  
+      <li>
+        <Link href="/csr">
+          <a>CSR</a>
+        </Link>
+      </li>                 
+    </ul>    
+    </>
+  );
+}
